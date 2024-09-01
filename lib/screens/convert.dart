12c823b072fc5bad77ee195b68278b17bda4dcf7
Phoobet <phoobets65@nu.ac.th>
2024-09-01 20:39:51 +0700

    Update convert.dart

--- lib/screens/convert.dart
+++ lib/screens/convert.dart
@@ -1,54 +1,62 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:simple_app/Currency.dart';
+import 'package:resume/Currency.dart';
 
 class Convert extends StatefulWidget {
   const Convert({super.key});
-
   @override
   State<Convert> createState() => _ConvertState();
 }
 
 class _ConvertState extends State<Convert> {
-
   @override
-  void initState() {
+  initState() {
     super.initState();
-    Future<Currency> result = getCurrency();
+    print("Init State");
   }
 
-  Future<Currency> getCurrency() async{
-    // var params = <String, String>{
-    //   'base': 'USD'
-    // };
-    var url = "https://currency-converter-pro1.p.rapidapi.com/currencies";
+  Future<Currency> getCurrency() async {
+    var uri =
+        Uri.parse("https://currency-converter-pro1.p.rapidapi.com/currencies");
 
-    var uri = Uri.https(url,'/currencies');
-
-    var response = await http.get(
-      uri,
-      headers: <String, String>{
-        'X-RapidAPI-Key': ' ebe77befcamsh4b38efb093e9848p1e09bejsn3b5365d6ede2',
-        'X-RapidAPI-Host': 'currency-converter-pro1.p.rapidapi.com'
-      },
-    );
-    Currency currency = currencyFromJson(response.body);
-    return currency;
-    
-    // return result;
-
+    var response = await http.get(uri, headers: {
+      "X-RapidAPI-Host": "currency-converter-pro1.p.rapidapi.com",
+      "x-rapidapi-key": "ebe77befcamsh4b38efb093e9848p1e09bejsn3b5365d6ed"
+    });
+    Currency result = currencyFromJson(response.body);
+    print(result.toString());
+    return result;
   }
 
   @override
   Widget build(BuildContext context) {
+    print("Build");
     return FutureBuilder(
       future: getCurrency(),
-      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-        if(snapshot.connectionState == ConnectionState.done){
-          return Text("Finished Load Data"); 
+      builder: (BuildContext context, AsyncSnapshot<Currency> snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text("Error: ${snapshot.error}"),
+            );
+          } else {
+            return ListView.builder(
+                itemCount: snapshot.data!.result!.length,
+                itemBuilder: (BuildContext context, int index) {
+                  String? key = snapshot.data?.result!.keys.elementAt(index);
+                  String? value = snapshot.data?.result![key];
+                  return ListTile(
+                    title: Text(key!),
+                    subtitle: Text(value!),
+                  );
+                });
+          }
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
         }
-        return CircularProgressIndicator();
-      });
+      },
+    );
   }
 }
