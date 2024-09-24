e693fdf1c2b73c6324cd60bc46cf8695e85293b7
Phoobet <phoobets65@nu.ac.th>
2024-09-24 11:18:46 +0700

    first commit00

---
+++ lib/provider/transaction_provider.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/foundation.dart';
+import 'package:simple_app/models/transaction.dart';
+
+class TransactionProvider with ChangeNotifier {
+  List<Transaction> transactions = [
+    
+  ];
+
+  List<Transaction> getTransaction() {
+    return transactions;
+  }
+
+  void addTransaction(Transaction transaction) {
+    transactions.add(transaction);
+    notifyListeners();
+  }
+
+  void deleteTransaction(int index) {
+    transactions.removeAt(index);
+    notifyListeners(); 
+  }
+}
